fc2af2f91c5292a5233e2e1cc6634fd74a0d38ce
Geetha Gandham <geethagandham07@gmail.com>
2026-01-23 00:44:54 +0530

    day2_variables_datatypes.py

---
+++ python/day2_variables_datatypes.py
@@ -0,0 +1,24 @@
+# Day 2 - Variables and Data Types
+
+# Integer
+a = 10
+b = 20
+print(a + b)
+print(type(a))
+
+# Float
+x = 10.5
+print(x)
+print(type(x))
+
+# String
+name = "Geetha"
+print(name)
+print(type(name))
+
+# Dynamic typing
+value = 10
+print(value, type(value))
+
+value = "Python"
+print(value, type(value))
