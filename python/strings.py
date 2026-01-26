70ba54b43d6d8bb1f4fba4b1f9a916069cb66c3e
geetha625 <geethagandham07@gmail.com>
2026-01-26 22:04:13 +0530

    added string reversal and palindrome problems

--- python/strings.py
+++ python/strings.py
@@ -73,5 +73,76 @@
 #txt=f"my name is geetha,i am {age}"
 #print(txt)
 
+#txt="geetha"
+#for i in txt:
+ #   x=txt.count(i)
+#print(x)
+ 
+#txt="geetha"
+#print(txt[0]+txt[:0:-1])  #reversing a string
+
+#print("python">"java")  #comparing strings
+#print("apple"<"banana")  #comparing strings
+
+#print("hello"=="Hello")  #comparing strings
+#print(not(False or True))
+
+#1.reverse a string without using built-in funcs
+#a="geetha"
+#print(a[::-1])
+
+#2.revere the string keeping the first char same
+#a="geetha"
+#print(a[0]+a[:-6:-1])
+
+#3.reverse the string keeping the last char same
+#a="geetha"
+#print(a[-1]+a[-2::-1])
+
+#4.reverse only words in a sentence,not the sentence itself
+#a="hello world"
+#print(a[-7::-1]+" "+a[-1:-7:-1])
+
+#5.reverse a string except special chars
+#a="g@ee#tha"
+#print(a[-1]+a[-7]+a[-2:-4:-1]+a[-4]+a[-5:-7:-1]+a[-8])
+
+#palindrome
+#6.check whether a string is a palindrome
+#a="wow"
+#if a==a[::-1]:
+#    print("palindrome")
+#else:
+ #   print("not a palindrome")
+
+#7.check whether a string is a palindrome or not without using slicing
+#a="madam"
+#rev=""
+#for i in a:
+ #   rev=i+rev
+#if a==rev:
+ #   print("palindrome")
+#else:
+  #  print("not a palindrome")
+
+#8.check palindrome ignoring case
+#a="Madam"
+#if a.upper()==a[::-1].upper():
+ #   print("palindrome")
+
+#9.check palindrome ignoring spaces 
+#a="nurses run"
+#a=a.replace(" ","")
+#if a==a[::-1]:
+ #   print("palindrome")
+
+    
+
+
+
+
+
+
+
 
 
