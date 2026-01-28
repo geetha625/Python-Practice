3ebccf155ee21c3dcd912bc37d84aa56df79fc31
geetha625 <geethagandham07@gmail.com>
2026-01-28 23:00:41 +0530

    python lists of list items,access items

---
+++ python/lists.py
@@ -0,0 +1,28 @@
+#thislist=["apple", "banana", "cherry"]
+#thislist[0] = "kiwi"
+#print(thislist[1])
+
+#thislist = ["apple", "banana", "cherry"]
+#thislist[1:3] = ["watermelon", "mango"]
+#print(thislist)
+
+#1.#create a list of 5 nums
+#mylist = [10, 20, 30, 40, 50]
+#print(mylist)
+
+#2.print first and last element
+#mylist=[10,20,30,40,50]
+#print(mylist[0])
+#print(mylist[-1])
+
+#3.change 3rd element value
+#mylist=[10,20,30,40,50]
+#mylist[2]=3
+#print(mylist)
+
+#4.print list using loop
+
+#5.check type of list elements
+mylist=[1,2,3,4,5]
+print(type(mylist))
+
---
+++ python/operators.py
@@ -0,0 +1,21 @@
+# operators
+x=3
+y=2
+print(x+y)  #addition
+print(x-y)  #subtraction
+print(x*y)  #multiplication
+print(x/y)  #division
+print(x%y)  #modulus
+print(x**y) #exponentiation
+print(x//y) #floor division
+
+#assignment operators
+x=5
+y=3
+print(x==y)  #equalto
+print(x!=y)  #not equal to
+print(x>y)   #greater than
+print(x<y)   #less than
+print(x>=y)  #greater than or equal to
+print(x<=y)  #less than or equal to
+
