dbd3a7f399f5b8570a37b23394686c86a4759c9b
geetha625 <geethagandham07@gmail.com>
2026-01-29 20:03:25 +0530

    practiced list operations

--- python/lists.py
+++ python/lists.py
@@ -23,6 +23,35 @@
 #4.print list using loop
 
 #5.check type of list elements
-mylist=[1,2,3,4,5]
-print(type(mylist))
+#mylist=[1,2,3,4,5]
+#print(type(mylist))
 
+
+#1.create a list of 5 nums
+#mylist=[1,2,3,4,5]
+#print(mylist)
+
+#2.add one num at the end
+#mylist=[1,2,3,4,5]
+#mylist.append(6)
+#print(mylist)
+
+#3.insert a num at index 2
+#mylist=[1,2,3,4,5]
+#mylist.insert(2,7)
+#print(mylist)
+
+#4.remove one element
+#mylist=[1,2,3,4,5]
+#mylist.remove(4)
+#print(mylist)
+
+#5.print all elements using loop
+#mylist=[1,2,3,4,5]
+#for i in mylist:
+ #   print(i)
+
+#6.check if 25 exists in the list
+mylist=[5,10,15,20,25]
+if 25 in mylist:
+    print("yes")
