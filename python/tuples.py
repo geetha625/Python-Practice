47b2451eac726b78ee42a6bc82b8779a39d5538a
geetha625 <geethagandham07@gmail.com>
2026-02-02 22:39:54 +0530

    basic tuple creation

---
+++ python/tuples.py
@@ -0,0 +1,11 @@
+#create a tuple with values 10,20,30,40 and print it
+#thistuple=(10,20,30,40)
+#print(thistuple)
+
+#create a tuple with 1 element 50 and print its type
+#thistuple=(50)
+#print(type(thistuple))
+
+#create a tuple containing int,float,string,boolean
+thistuple=(10,2.5,"geetha","true")
+print(thistuple)
