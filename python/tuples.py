831ea0e1539db190a4a7f32be120bf6c32fa2cfe
geetha625 <geethagandham07@gmail.com>
2026-02-04 23:08:14 +0530

    practiced update tuples,unpacking tuples,join tuples

--- python/tuples.py
+++ python/tuples.py
@@ -60,11 +60,140 @@
 #print(t)
 
 #print the sum of all elements in a tuple
-t=(10,20,30)
-total=0
-for i in t:
-    total+=i
-print(total)
-    
+#t=(10,20,30)
+#total=0
+#for i in t:
+#    total+=i
+#print(total)
+
+#x=("apple","banana","cherry")
+#y=list(x)
+#y[1]="kiwi"
+#x=tuple(y)
+#print(x)
+
+#x=("apple","banana","cherry")
+#y=list(x)
+#y.append("orange")
+#x=tuple(y)
+#print(x)   
+
+#x=(1,2,3,4) 
+#y=(5,)
+#x+=y
+#print(x)
+
+#x=(1,2,3,4)
+#y=list(x)
+#y.remove(3)
+#x=tuple(y)
+#print(x)
+
+#x=(1,2,3)
+#del x
+#print(x)
+
+#fruits=("apple","banana","cherry")
+#(green,yellow,red)=fruits
+#print(green)
+#print(yellow)
+#print(red)
+
+#fruits=("apple","banana","cherry","melon")
+#(green,yellow,*red)=fruits
+#print(green)
+#print(yellow)
+#print(red)
+
+#a=("a","b","c")
+#b=(1,2,3)
+#c=a+b
+#print(c)
+
+#a=("a","b","c")
+#b=a*2
+#print(b)
+
+#1.replace an element in a tuple
+#t=(10,20,30,40)
+#x=list(t)
+#x.remove(20)
+#x.insert(1,25)
+#t=tuple(x)
+#print(t)
+
+#2.add an element to a tuple
+#t=(1,2,3)
+#x=list(t)
+#x.append(50)
+#t=tuple(x)
+#print(t)
+
+#3.remove an element from a tuple
+#t=(10,20,30,40)
+#x=list(t)
+#x.remove(30)
+#t=tuple(x)
+#print(t)
+
+#4.add 5 to the list inside the tuple
+#t=(1,[2,3],4)
+#x=list(t)
+#x.append(5)
+#t=tuple(x)
+#print(t)
+
+#5.unpack into a,b,c and print them
+#t=(10,20,30)
+#(a,b,c)=t
+#print(a)
+#print(b)
+#print(c)
+
+#6.swap 2 nums using tuple unpacking
+#a=5
+#b=10
+#(a,b)=(b,a)
+#print(a)
+#print(b)
+
+#7.unpack with * operator
+#t=(1,2,3,4,5)
+#(a,b,*c)=t
+#print(a)
+#print(b)
+#print(*c)
+
+#8.ignore values while unpacking
+#t=(10,20,30,40)
+#(a,_,_,d)=t
+#print(a)
+#print(d)
+
+#9.join 2 tuples
+#t1=(1,2,3)
+#t2=(4,5)
+#t3=t1+t2
+#print(t3)
+
+#10.join tuple multiple times
+#t=(1,2)
+#t=t*3
+#print(t)
+
+#11.join tuple and list
+#t=(1,2)
+#l=[3,4]
+#m=tuple(l)
+#x=t+m
+#print(x)
+
+#12.predit the output
+t=(1,2,3)
+t+=(4,)
+print(t)
+
+
+
  
 
