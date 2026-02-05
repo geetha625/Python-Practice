9e727fa2790628d58b652122f3b4d46ae45a3530
geetha625 <geethagandham07@gmail.com>
2026-02-05 23:05:06 +0530

    practiced sets:basics,add(),update() methods

---
+++ python/sets.py
@@ -0,0 +1,32 @@
+#write a program to print all elements of a set using a loop
+#x={1,2,3}
+#for i in x:
+#    print(i)
+
+#check whether an element exists in a set
+#x={1,2,3,4,5}
+#if 3 in x:
+#    print("yes")
+#else:
+#    print("no")
+
+#check output
+#s={10,20,30}
+#print(40 in s)
+
+#write a program to add one element to a set
+#s={10,20,30,40}
+#s.add(50)
+#print(s)
+
+#add multiple elements
+#s={10,20,30,40}
+#t={"a","b","c"}
+#s.update(t)
+#print(s)
+
+#check output
+s={1,2,3}
+s.add(2)
+print(s)
+
