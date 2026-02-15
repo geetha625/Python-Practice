4d2446a78322173d2623f6d67b7372bf55705abf
geetha625 <geethagandham07@gmail.com>
2026-02-15 14:45:43 +0530

    practiced increasing in patterns

---
+++ python/patterns.py
@@ -0,0 +1,31 @@
+#no of rows=n
+#to print in each row :stars,nums,alphabets
+
+#square
+#n=4
+#for i in range(n):
+#    for j in range(n):
+#        print("*",end="")
+#    print()
+
+#n=5
+#for i in range(1,n+1):
+#    print("*"*i)
+
+#n=5
+#for i in range(1,n+1):
+#    for j in range(1,i+1):
+#        print(j,end="")
+#    print()
+
+#n=5
+#for i in range(1,n+1):
+#    for j in range(2,i+2):
+#        print(j,end="")
+#    print()
+
+n=5
+for i in range(1,n+1):
+    for j in range(3,i+3):
+        print(j,end="")
+    print()
