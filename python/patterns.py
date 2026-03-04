a4c8f001cc2f05345a569738c5f2bd5a0d7d6322
geetha625 <geethagandham07@gmail.com>
2026-03-04 20:53:13 +0530

    patterns practice

--- python/patterns.py
+++ python/patterns.py
@@ -24,8 +24,8 @@
 #        print(j,end="")
 #    print()
 
-n=5
-for i in range(1,n+1):
-    for j in range(3,i+3):
-        print(j,end="")
-    print()
+#n=5
+#for i in range(1,n+1):
+#    for j in range(3,i+3):
+#        print(j,end="")
+#    print()
