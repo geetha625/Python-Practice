8463ba80fc903debf294179d0dd68d095ffb35c0
geetha625 <geethagandham07@gmail.com>
2026-03-15 22:04:36 +0530

    increase numbers pattern

--- python/patterns.py
+++ python/patterns.py
@@ -57,6 +57,8 @@ for i in range(n):
         print("*"+spaces+"*")'''
 
 # same num in row             - row num = num to print
+# one loop for rows   - i=row num
+# one loop for nums inside each row     - j=num to print
 '''n=4
 for i in range(1,n+1):
     for j in range(i):
@@ -68,17 +70,34 @@ for i in range(1,n+1):
 3 3 3
 4 4 4 4 '''
 
-n=5
+'''n=5
 for i in range(1,n+1):
     for j in range(i):
         print(i,end=" ")
-    print()
+    print() '''
 # output :
 '''1
 2 2 
 3 3 3
 4 4 4 4 
 5 5 5 5 5'''
+
+# increasing number pattern 
+'''n=4
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+1 
+1 2 
+1 2 3
+1 2 3 4 '''
+
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
 
 
                                  
