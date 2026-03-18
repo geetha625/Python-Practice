911e9cdc9aff837b1becca24c78696f9467ce515
geetha625 <geethagandham07@gmail.com>
2026-03-18 21:59:07 +0530

    in functions

--- python/poweroftwo.py
+++ python/poweroftwo.py
@@ -1,6 +1,30 @@
 
-n=int(input("enter a number:"))    
+'''n=int(input("enter a number:"))    
 if n>0 and (n&(n-1)==0):
     print("yes")
 else:
-    print("false")
+    print("false")'''
+
+def isPowerofTwo(n):
+    if n<=0:
+        return False
+    return (n&(n-1))==0
+n=int(input("enter a num:"))
+print(isPowerofTwo(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
