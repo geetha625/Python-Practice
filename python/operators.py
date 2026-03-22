3c4c3c7a1a8445b448963743214dda5e40195dc1
geetha625 <geethagandham07@gmail.com>
2026-03-22 18:36:42 +0530

    revised operators

--- python/operators.py
+++ python/operators.py
@@ -9,7 +9,7 @@ print(x%y)  #modulus
 print(x**y) #exponentiation
 print(x//y) #floor division
 
-#assignment operators
+# comparision operators           ( used to assign values to variables)
 x=5
 y=3
 print(x==y)  #equalto
@@ -18,4 +18,31 @@ print(x>y)   #greater than
 print(x<y)   #less than
 print(x>=y)  #greater than or equal to
 print(x<=y)  #less than or equal to
+
+# assignment operators  (must be on their own line not inside print())
+x,y=5,8
+x=y
+print(x)  # 8
+x+=y           # x=x+y
+print(x)  # 16
+x-=y           # x=x-y
+print(x)  # 8
+x*=y           # x=x*y
+print(x)  # 64
+x%=y          
+print(x)   # 0
+
+# logical operators        ( used to combine conditional statements)
+x=5
+y=8
+print(x>y and x!=y)
+print(x and y)
+print(x or y)
+print(not y)
+
+# membership operators    ( used to check the sequence in objects)
+x=[1,2,3,4,5]
+y=4
+print(y in x)
+print(y not in x)
 
