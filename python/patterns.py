ad0d155bf4a1609589f2bef1a6c2bd6dd711e196
geetha625 <geethagandham07@gmail.com>
2026-03-23 22:01:15 +0530

    patterns

--- python/patterns.py
+++ python/patterns.py
@@ -201,12 +201,30 @@ OUTPUT:
   * * * *
 * * * * * '''
 
-n=5
-for i in range(1,n+1):
-    for j in range(i):
-        print("*",end=" ")
+'''n=5
+for i in range(n,0,-1):
     for j in range(n-i):
         print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
     print()
+#OUTPUT:
+* * * * * 
+  * * * * 
+    * * *
+      * *
+        * '''
 
+'''n=4
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
+OUTPUT:
+      * 
+    * * * 
+  * * * * *
+* * * * * * * '''
 
